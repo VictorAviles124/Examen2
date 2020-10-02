5c0f79b448c4dee32cbe4aa1d2d824b41e2c4658
Victor <shareedq123@gmail.com>
2020-10-02 10:27:02 -0500

    primer commit

---
+++ contenido.php
@@ -0,0 +1,27 @@
+<?php
+
+function contenido(){
+    $datos = array();
+             $datos[0]="img/iphone11pro.png"."||".
+            "iPhone 11 PRO"."||".
+            "Tres cámaras: ultra gran angular, gran angular y teleobjetivo. Modo Noche, Super Retina XDR, A13 Bionic y hasta 5 horas más de batería.";
+            
+            $datos[1]="img/iphone11.png"."||".
+            "iPhone 11"."||".
+            "Liquid Retina LCD. Chip A13 Bionic. Batería para todo el día. Cámara ultra gran angular. Sistema de dos cámaras.
+            ";
+            $datos[2]="img/iphonese.png"."||".
+            "iPhone SE"."||".
+            "viene con el chip A13 Bionic, modo Retrato, video 4K, Touch ID, pantalla Retina HD y una batería de gran duración en un diseño de 4.7”..
+            ";
+
+            $datos[3]="img/iphonexr.png"."||".
+            "iPhone XR"."||".
+            " Tiene una pantalla Liquid Retina, cámara TrueDepth, Face ID y el chip A12 Bionic.
+            ";
+            $datos[4]="img/ipad.png"."||".
+            "iPad"."||".
+            "tiene el chip A12 Bionic con Neural Engine y pantalla Retina de 7.9 pulgadas en un diseño compacto, y es compatible con el Apple Pencil.";
+
+            return $datos;
+}
---
+++ dependecias.php
@@ -0,0 +1,12 @@
+<meta name="viewport" content="width=device-widht, initial-scale=1,
+maximun-scale=1, user-scalable=no">
+
+<meta charset="utf-8">
+<link rel="stylesheet" href="librerias/bootstrap/css/bootstrap.css">
+<link rel="stylesheet" href="librerias/gridder/css/jquery.gridder.min.css">
+<link rel="stylesheet" href="librerias/font-awesome/css/font-awesome.css">
+<link rel="stylesheet" href="css/demo.css">
+
+<script src="librerias/jquery-3.3.1.min.js"></script>
+<script src="librerias/bootstrap/js/bootstrap.js"></script>
+<script src="librerias/gridder/js/jquery.gridder.min.js"></script>s
